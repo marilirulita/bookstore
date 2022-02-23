416011e2444cf860d9d4cc85ad9cdb6f7221a84d
marysol <marilirulita@gmail.com>
2022-02-22 18:14:38 -0800

    Add unique id

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,16 @@
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
 
+  let smallStore = '';
   const submitBookToStore = () => {
     const newBook = {
-      title: 'First Book',
+      title: smallStore,
       author: 'Anonimus',
-      id: 1,
+      id: uuidv4(),
     };
 
     dispatch(addBook(newBook));
@@ -16,7 +18,7 @@ const AddBook = () => {
 
   return (
     <div className="add-book">
-      <input type="text" placeholder="Add new book" />
+      <input type="text" placeholder="Add new book" onChange={(e) => { smallStore = e.target.value; }} />
       <button type="submit" value="add-book" onClick={submitBookToStore}>Add</button>
     </div>
   );
