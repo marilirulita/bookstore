4d531b49889a9ed3b45dbcb8e4439d3904fef912
marysol <marilirulita@gmail.com>
2022-02-22 20:30:18 -0800

    Add usestate hook add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,24 +1,28 @@
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
+  const [state, setState] = useState('');
+
   const dispatch = useDispatch();
 
-  let smallStore = '';
   const submitBookToStore = () => {
     const newBook = {
-      title: smallStore,
+      title: state,
       author: 'Anonimus',
       id: uuidv4(),
     };
 
     dispatch(addBook(newBook));
+
+    setState('');
   };
 
   return (
     <div className="add-book">
-      <input type="text" placeholder="Add new book" onChange={(e) => { smallStore = e.target.value; }} />
+      <input type="text" placeholder="Add new book" value={state} onChange={(e) => setState(e.target.value)} />
       <button type="submit" value="add-book" onClick={submitBookToStore}>Add</button>
     </div>
   );
