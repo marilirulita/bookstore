d9237ba51ce5270d1c645c107f6c4111110b3c33
marysol <marilirulita@gmail.com>
2022-02-22 21:41:26 -0800

    Add object to manaje state

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,25 +4,28 @@ import { useState } from 'react';
 import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
-  const [state, setState] = useState('');
-
+  const initState = {
+    title: '',
+    author: '',
+  };
+  const [state, setState] = useState(initState);
   const dispatch = useDispatch();
 
   const submitBookToStore = () => {
     const newBook = {
-      title: state,
-      author: 'Anonimus',
+      title: state.title,
+      author: state.author,
       id: uuidv4(),
     };
 
     dispatch(addBook(newBook));
 
-    setState('');
+    setState(initState);
   };
 
   return (
     <div className="add-book">
-      <input type="text" placeholder="Add new book" value={state} onChange={(e) => setState(e.target.value)} />
+      <input type="text" placeholder="Add new book" value={state.title} onChange={(e) => setState({ title: e.target.value, author: 'Anonimus' })} />
       <button type="submit" value="add-book" onClick={submitBookToStore}>Add</button>
     </div>
   );
