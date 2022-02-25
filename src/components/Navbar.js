7d97986ebe12ced9dbb62745c3148b4368e90dc1
marysol <marilirulita@gmail.com>
2022-02-25 14:51:24 -0800

    Add icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 /* eslint-disable react/jsx-key */
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { ImUser } from "react-icons/im";
 
 const Navbar = () => (
   <div className="navbar">
     <div className="links">
       <h1 className="Bookstore-CMS1">Bookstore CMS</h1>
-      <Link to="/" className="BOOKS">BOOKS</Link>
-      <Link to="/categories" className="CATEGORIES">CATEGORIES</Link>
+      <Link to="/" className="BOOKS">
+        BOOKS
+      </Link>
+      <Link to="/categories" className="CATEGORIES">
+        CATEGORIES
+      </Link>
     </div>
     <div className="Oval">
-      <div className="Mask" />
+      <ImUser className="Mask" />
     </div>
   </div>
 );
