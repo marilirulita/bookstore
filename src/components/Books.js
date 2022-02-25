2f17cd285fcb208a87186a675f5386fc378612ce
marysol <marilirulita@gmail.com>
2022-02-24 16:10:51 -0800

    Add get appidata to reducer

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchApiData } from '../redux/books/books';
 import AddBook from './AddBook';
-import DeleteBook from './DeleteBook';
+// import DeleteBook from './DeleteBook';
 
 const Books = () => {
   const books = useSelector((state) => state.booksReducer);
@@ -10,7 +10,6 @@ const Books = () => {
 
   useEffect(() => {
     dispatch(fetchApiData());
-    console.log(books);
   }, []);
 
   return (
@@ -18,7 +17,7 @@ const Books = () => {
       <h2>This is a bookstore</h2>
       <AddBook />
       <ul>
-        {books.map((book) => (<DeleteBook key={book.id} item={book} />))}
+        {/* {books.map((book) => (<DeleteBook key={book.id} item={book} />))} */}
       </ul>
     </div>
   );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,3 @@
-// import axios from 'axios';
-
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const GET_API_DATA = 'bookStore/books/GET_API_DATA';
@@ -17,37 +15,22 @@ export const removeBook = (payload) => ({
   payload,
 });
 
-// export const fetchApiData = () => {
-//   return (dispatch) => {
-//     dispatch(fetchUserRequest) // ???
-//     axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/A8pinz5DqOreWDXV7Uvo/books/')
-//     .then(response => {
-//       const books = response.data;
-//       dispatch(fetchUserSucces(books)) // ????
-//     })
-//     .catch(error => {
-//       const errorMsg = error.message
-//       dispatch(fetchUserFailer(errorMsg)) // ???
-//     })
-//   }
-// }
+export const fetchApiData = () => async (dispatch) => {
+  const result = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/A8pinz5DqOreWDXV7Uvo/books/',
+  );
+  const resultJson = await result.json();
+  if (resultJson) {
+    const objeArr = Object.keys(resultJson).map((key) => ({
+      id: key,
+      author: 'Anonimous',
+      ...resultJson[key][0],
+    }));
 
-export const fetchApiData = () => {
-  try {
-    return async (dispatch) => {
-      const result = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/A8pinz5DqOreWDXV7Uvo/books/');
-      const resultJson = await result.json();
-      if (resultJson) {
-        dispatch({
-          type: GET_API_DATA,
-          payload: resultJson,
-        });
-      } else {
-        console.log('Unable to fetch!');
-      }
-    };
-  } catch (error) {
-    console.log(error);
+    dispatch({
+      type: GET_API_DATA,
+      payload: objeArr,
+    });
   }
 };
 
@@ -55,23 +38,14 @@ export const fetchApiData = () => {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [
-        ...state, action.payload,
-      ];
+      return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
     case GET_API_DATA:
-      return [
-        ...state, action.payload,
-      ];
+      return [...state, ...action.payload];
     default:
       return state;
   }
 };
-
-// side effects, only as applicable
-// export function getBook() {
-//   return (dispatch) => get('/book').then((book) => dispatch(updateBook(book)));
-// }
 
 export default reducer;
